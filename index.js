898e6d0ee92084dc45998ee8b9ae498c0fc152d4
junghyundo <jhd4271@gmail.com>
2025-09-29 23:44:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => res.send("Express on Vercel"));
+
+// app.listen(3000, () => console.log("Server ready on port 3000."));
+
+// module.exports = app;
+
+const express = require('express');
 const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// app.use(express.static());
+app.use('/', express.static(__dirname+"/"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// Home 페이지 라우트
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-module.exports = app;
+// About 페이지 라우트
+// app.get('/detail', (req, res) => {
+//   res.sendFile(__dirname + '/detail.html');
+// });
+
+// Product 페이지 라우트
+// app.get('/result', (req, res) => {
+//   res.sendFile(__dirname + '/result.html');
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
