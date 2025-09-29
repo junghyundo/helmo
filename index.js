ef1e3f275b1e82cfe3b18c203758f9f468af037d
junghyundo <jhd4271@gmail.com>
2025-09-29 23:58:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
 // app.listen(3000, () => console.log("Server ready on port 3000."));
 
 // module.exports = app;
-
 const express = require('express');
 const app = express();
+const port = 3000;
 
 // app.use(express.static());
-// app.use('/', express.static(__dirname+"/"));
+app.use('/', express.static(__dirname+"/"));
 
 // Home 페이지 라우트
 app.get('/', (req, res) => {
@@ -27,3 +27,7 @@ app.get('/', (req, res) => {
 // app.get('/result', (req, res) => {
 //   res.sendFile(__dirname + '/result.html');
 // });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
